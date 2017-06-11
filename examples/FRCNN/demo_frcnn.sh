bc1dcd648b7f7d8825b8c219b3b0bc78071e1565
Ubuntu <stingshen@UbuntuCheap.da0lergrcxpu1ogg3fbof1wrpf.ix.internal.cloudapp.net>
2017-06-11 03:53:37 +0000

    Fix demo_frcn.sh

--- examples/FRCNN/demo_frcnn.sh
+++ examples/FRCNN/demo_frcnn.sh
@@ -3,18 +3,20 @@
 # determine whether $1 is empty
 if [ ! -n "$1" ] ;then
     echo "no parameter, default is 0"
-    gpu=0
+    gpu="--gpu 0"
 elif ["-no-gpu" == "$1"] ;then
     echo "don't use gpu"
     gpu=""
 else
     echo "use $1-th gpu"
-    gpu=$1
+    gpu="--gpu $1"
 fi
+
+source models/FRCNN/fetch_imagenet_models.sh
 
 BUILD=build/examples/FRCNN/demo_frcnn_api.bin
 
-$BUILD --gpu $gpu \
+$BUILD $gpu \
        --model models/FRCNN/vgg16/test.proto \
        --weights models/FRCNN/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel \
        --default_c examples/FRCNN/config/voc_config.json \
