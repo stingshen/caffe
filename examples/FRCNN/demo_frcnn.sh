52d63ece822bbee25e9120f583098c9d719e9faf
Ubuntu <stingshen@UbuntuCheap.da0lergrcxpu1ogg3fbof1wrpf.ix.internal.cloudapp.net>
2017-06-11 02:39:21 +0000

    Update docker file and demo frcnn script.

--- examples/FRCNN/demo_frcnn.sh
+++ examples/FRCNN/demo_frcnn.sh
@@ -2,8 +2,11 @@
 # This script test four voc images using faster rcnn end-to-end trained model (ZF-Model)
 # determine whether $1 is empty
 if [ ! -n "$1" ] ;then
-    echo "$1 is empty, default is 0"
+    echo "no parameter, default is 0"
     gpu=0
+elif ["-no-gpu" == "$1"] ;then
+    echo "don't use gpu"
+    gpu=""
 else
     echo "use $1-th gpu"
     gpu=$1
@@ -12,8 +15,8 @@ fi
 BUILD=build/examples/FRCNN/demo_frcnn_api.bin
 
 $BUILD --gpu $gpu \
-       --model models/FRCNN/vgg16/test.prototxt \
-       --weights models/FRCNN/VGG16_faster_rcnn_final.caffemodel \
+       --model models/FRCNN/vgg16/test.proto \
+       --weights models/FRCNN/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel \
        --default_c examples/FRCNN/config/voc_config.json \
        --image_dir examples/FRCNN/images/  \
        --out_dir examples/FRCNN/results/ 
