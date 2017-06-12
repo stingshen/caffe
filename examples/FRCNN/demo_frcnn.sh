1e5438e76d3ab359fe0e6bd6c0ed295d29fd4af4
stingshen <stingshen1@gmail.com>
2017-06-12 22:59:13 +0800

    Fix frcnn demo scripts.

--- examples/FRCNN/demo_frcnn.sh
+++ examples/FRCNN/demo_frcnn.sh
@@ -2,18 +2,25 @@
 # This script test four voc images using faster rcnn end-to-end trained model (ZF-Model)
 # determine whether $1 is empty
 if [ ! -n "$1" ] ;then
-    echo "$1 is empty, default is 0"
-    gpu=0
+    echo "no parameter, default is 0"
+    gpu="--gpu 0"
+elif [ "--no-gpu" == "$1" ] ;then
+    echo "don't use gpu"
+    gpu=""
 else
     echo "use $1-th gpu"
-    gpu=$1
+    gpu="--gpu $1"
 fi
+
+pushd models/FRCNN/
+source fetch_imagenet_models.sh
+popd
 
 BUILD=build/examples/FRCNN/demo_frcnn_api.bin
 
-$BUILD --gpu $gpu \
+$BUILD $gpu \
        --model models/FRCNN/vgg16/test.proto \
        --weights models/FRCNN/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel \
        --default_c examples/FRCNN/config/voc_config.json \
        --image_dir examples/FRCNN/images/  \
-       --out_dir examples/FRCNN/results/ 
+       --out_dir examples/FRCNN/results/
--- models/FRCNN/fetch_imagenet_models.sh
+++ models/FRCNN/fetch_imagenet_models.sh
@@ -3,7 +3,8 @@
 FILE=faster_rcnn_models.tgz
 #URL=https://dl.dropbox.com/s/gstw7122padlf0l/imagenet_models.tgz?dl=0
 URL=https://dl.dropboxusercontent.com/s/o6ii098bu51d139/faster_rcnn_models.tgz?dl=0
-CHECKSUM=ed34ca912d6782edfb673a8c3a0bda6d
+#CHECKSUM=ed34ca912d6782edfb673a8c3a0bda6d
+CHECKSUM=ac116844f66aefe29587214272054668
 
 if [ -f $FILE ]; then
   echo "File already exists. Checking md5..."
